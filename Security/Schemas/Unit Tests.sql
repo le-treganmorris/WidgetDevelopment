aeb007b7e8c3f5ac795d6766c3d8b60169f758c8
Tim Regan-Morris <treganmorris@lizearle.com>
2016-01-12 15:13:01 +0000

    Remove all

--- Security/Schemas/Unit Tests.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA [Unit Tests]
-AUTHORIZATION [dbo]
-GO
-DECLARE @xp int
-SELECT @xp=1
-EXEC sp_addextendedproperty N'tSQLt.TestClass', @xp, 'SCHEMA', N'Unit Tests', NULL, NULL, NULL, NULL
-GO
